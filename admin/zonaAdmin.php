34dccec9e91f8bb6e705bd029eae35bccd8db432
Ruben Clase <ruben.serrano@iesdoctorbalmis.com>
2024-10-24 11:15:08 +0200

    cambio idioma

--- admin/zonaAdmin.php
+++ admin/zonaAdmin.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>Pagina Admin</h1>
+    <form action="" method="post" name="administradorUsuario"></form>
+    <form action="" method="post" name="administradorCartas"></form>
 </body>
 </html>
